f84cf5570b670609c02e17f82c6e73ccac842e84
Kim Branzell <hej@jagheter.kim>
2021-03-05 21:36:39 +0100

    Migrate from jade to pug

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ var io = require('socket.io').listen(server);
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(methodOverride());
